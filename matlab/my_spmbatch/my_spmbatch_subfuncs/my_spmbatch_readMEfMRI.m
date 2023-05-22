1d4cc2ae5d6d2f655280626e58d3b3abf7426bc3
dr. Van Schuerbeek P <60138268+P-VS@users.noreply.github.com>
2023-05-22 17:03:16 +0200

    Add files via upload

--- matlab/my_spmbatch/my_spmbatch_subfuncs/my_spmbatch_readMEfMRI.m
+++ matlab/my_spmbatch/my_spmbatch_subfuncs/my_spmbatch_readMEfMRI.m
@@ -9,7 +9,7 @@ for i=1:params.nechoes
     jsondat = fileread(funcjsonfile);
     jsondat = jsondecode(jsondat);
 
-    te(i) = jsondat.EchoTime;
+    te(i) = 1000.0*jsondat.EchoTime;
 
     Vfunc = spm_vol(funcfile);
 
@@ -34,75 +34,71 @@ for i=1:params.nechoes
 
     efuncdat = spm_read_vols(Vfunc);
 
-    if params.combination=='TE_weighted'
-        if i==1
-            voldim = Vfunc.dim;
-            funcdat = zeros(voldim(1),voldim(2),voldim(3),numel(Vfunc));
-        end
-
-        funcdat = funcdat+efuncdat.*te(i);
-
-    elseif params.combination=='T2_weighted'
-        if i==1
-            voldim = Vfunc.dim;
-            tefuncdat = zeros(params.nechoes,voldim(1)*voldim(2)*voldim(3),numel(Vfunc));
-        end
-
-        tefuncdat(i,:,:) = reshape(efuncdat,[voldim(1)*voldim(2)*voldim(3),numel(Vfunc)]);
+    if i==1
+        voldim = Vfunc.dim;
+        tefuncdat = zeros(voldim(1)*voldim(2)*voldim(3),numel(Vfunc),params.nechoes);
     end
+
+    tefuncdat(:,:,i) = reshape(efuncdat,[voldim(1)*voldim(2)*voldim(3),numel(Vfunc)]);
 end
 
-if params.combination=='TE_weighted'
-    funcdat = funcdat./sum(te);
+weights = zeros(voldim(1)*voldim(2)*voldim(3),params.nechoes);
 
+if params.combination=='TE_weighted'
+    for ne=1:params.nechoes
+        weights(:,ne) = repmat(te(ne),voldim(1)*voldim(2)*voldim(3),1);
+    end
 elseif params.combination=='T2_weighted'
     %based on https://github.com/jsheunis/fMRwhy/tree/master
 
-    funcdat = zeros(voldim(1)*voldim(2)*voldim(3),numel(Vfunc));
-
     % Create "design matrix" X
-    X = horzcat(ones(params.nechoes,1), -te(:)*1000);
+    X = horzcat(ones(params.nechoes,1), -te(:));
 
-    for ti=1:numel(Vfunc)
-        t2star = zeros(voldim(1)*voldim(2)*voldim(3),1);
-        wt2star = zeros(voldim(1)*voldim(2)*voldim(3),params.nechoes);
+    t2star = zeros(voldim(1)*voldim(2)*voldim(3),1);
 
-        thres = 0.02*max(tefuncdat(1,:,ti),[],'all');
-        mask_ind = find(tefuncdat(1,:,ti)>thres);
+    thres = 0.02*max(tefuncdat(:,1,1),[],'all');
+    mask_ind = find(tefuncdat(:,1,1)>thres);
 
-        Y=[];
-        for ne=1:params.nechoes
-            Y=[Y;tefuncdat(ne,mask_ind,ti)];
-        end
-        Y = max(Y, 1e-11);
+    Y=[];
+    for ne=1:params.nechoes
+        Y=[Y;reshape(tefuncdat(mask_ind,1,ne),[1,numel(mask_ind)])];
+    end
+    Y = max(Y, 1e-11);
 
-        % Estimate "beta matrix" by solving set of linear equations
-        beta_hat = pinv(X) * log(Y);
-         % Calculate S0 and T2star from beta estimation
-        T2star_fit = 1 ./ beta_hat(2, :);
+    % Estimate "beta matrix" by solving set of linear equations
+    beta_hat = pinv(X) * log(Y);
+     % Calculate S0 and T2star from beta estimation
+    T2star_fit = 1 ./ beta_hat(2, :);
 
-        T2star_thresh_max = 1500; % arbitrarily chosen
-        T2star_thresh_min = 0; % arbitrarily chosen, same as tedana
-        I_T2star_min = (T2star_fit < T2star_thresh_min); % vector of voxels where T2star value is negative
-        T2star_fit(I_T2star_min) = 0; % if values inside mask are zero or negative, set them to threshold_min value
-        I_T2star_max =  T2star_fit(:) >= T2star_thresh_max; % vector of voxels in mask where T2star value is higher than threshold_max
-        T2star_fit(I_T2star_max) = 0; % set to zero, different from tedana where they set it to threshold_max value
+    T2star_thresh_max = 5000; % arbitrarily chosen
+    T2star_thresh_min = 0; % arbitrarily chosen, same as tedana
+    I_T2star_min = (T2star_fit < T2star_thresh_min); % vector of voxels where T2star value is negative
+    T2star_fit(I_T2star_min) = 0; % if values inside mask are zero or negative, set them to threshold_min value
+    I_T2star_max =  T2star_fit(:) >= T2star_thresh_max; % vector of voxels in mask where T2star value is higher than threshold_max
+    T2star_fit(I_T2star_max) = 0; % set to zero, different from tedana where they set it to threshold_max value
 
-        t2star(mask_ind)=T2star_fit;
-        wmask = find(t2star>0);
+    t2star(mask_ind)=T2star_fit;
+    wmask = find(t2star>0);
 
-        for ne=1:params.nechoes
-            wt2star(wmask,ne)=te(ne).*exp(-te(ne)./t2star(wmask,1));
+    for ne=1:params.nechoes
+        weights(wmask,ne)=te(ne).*exp(-te(ne)./t2star(wmask,1));
+    end
+end
 
-            funcdat(wmask,ti) = funcdat(wmask,ti)+tefuncdat(ne,wmask,ti)' .* wt2star(wmask,ne);
-        end
+sum_weights = sum(weights,2);
+weights_mask = find(sum_weights>0);
 
-        funcdat(wmask,ti) = funcdat(wmask,ti) ./ sum(wt2star(wmask,:),2);
+funcdat = zeros(voldim(1)*voldim(2)*voldim(3),numel(Vfunc));
+
+for ti=1:numel(Vfunc)
+    for ne=1:params.nechoes
+        funcdat(weights_mask,ti) = funcdat(weights_mask,ti)+(reshape(tefuncdat(weights_mask,ti,ne),[numel(weights_mask),1]) .* weights(weights_mask,ne));
     end
 
-    funcdat = reshape(funcdat,[voldim(1),voldim(2),voldim(3),numel(Vfunc)]);
-
+    funcdat(weights_mask,ti) = funcdat(weights_mask,ti) ./ sum_weights(weights_mask);
 end
+
+funcdat = reshape(funcdat,[voldim(1),voldim(2),voldim(3),numel(Vfunc)]);
 
 fname=Vfunc(1).fname;
 nfname = split(fname,['_e' num2str(params.nechoes)]);
