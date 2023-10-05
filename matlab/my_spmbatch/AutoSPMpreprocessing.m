d5d6330ed03ff25c77c44a10eb4094b09a5f3eed
dr. Van Schuerbeek P <60138268+P-VS@users.noreply.github.com>
2023-10-05 19:21:10 +0200

    Add files via upload

--- matlab/my_spmbatch/AutoSPMpreprocessing.m
+++ matlab/my_spmbatch/AutoSPMpreprocessing.m
@@ -38,16 +38,16 @@ SPMid                 = spm('FnBanner',mfilename,'2.10');
 
 datpath = '/Volumes/LaCie/UZ_Brussel/ME_fMRI_GE/data';
 
-sublist = [1,2,4:10];%list with subject id of those to preprocess separated by , (e.g. [1,2,3,4]) or alternatively use sublist = [first_sub:1:last_sub]
+sublist = [2];%list with subject id of those to preprocess separated by , (e.g. [1,2,3,4]) or alternatively use sublist = [first_sub:1:last_sub]
 nsessions = [1]; %nsessions>0
 
-params.save_folder = 'preproc_func_me-perTE';
+params.save_folder = 'preproc_func_me-ave_acompcor_test';
 
-task ={'ME-EmoFaces'};
+task ={'ME-EFT'};
 
 params.meepi = true;
 params.echoes = [1,2,3]; %number of echoes for ME-fMRI. 
-params.combination = 'none'; 
+params.combination = 'average'; 
 %none: all echoes are preprocessed separatly
 %average: The combination is the average of the multiple echo images
 %TE_weighted: The combination is done wi=TEi or 
@@ -74,16 +74,20 @@ params.do_smoothing = true;
 params.normvox = [1.5 1.5 1.5];
 params.smoothfwhm = 6;
 
-params.do_bpfilter = false;
-params.do_mot_derivatives = false; %derivatives+squares (24 regressors)
-params.do_aCompCor = false;
-params.do_noiseregression = false;
+params.do_onlydenoise = false; %only denoising and smoothing
+params.prefix = 'wcaure';
+params.mecombined = true;
+
+params.do_bpfilter = true;
+params.do_mot_derivatives = true; %derivatives+squares (24 regressors)
+params.do_aCompCor = true;
 params.do_ICA_AROMA = false;
+params.do_noiseregression = true;
 
 params.bpfilter = [0.008 Inf]; %no highpass filter is first 0, no lowpass filter is last Inf, default is [0.008 0.1]
-params.Ncomponents = 5; %if in range [0 1] then the number of aCompCor components is equal to the number of components that explain the specified percentage of variatiion in the signal
+params.Ncomponents = 0.5; %if in range [0 1] then the number of aCompCor components is equal to the number of components that explain the specified percentage of variation in the signal
 
-use_parallel = true;
+params.use_parallel = false;
 save_intermediate_results = false; %only possible when parallel toolbox is installed
 
 %% BE CAREFUL WITH CHANGING THE CODE BELOW THIS LINE !!
@@ -94,7 +98,7 @@ curdir = pwd;
 
 spm('defaults', 'FMRI');
 
-if use_parallel
+if params.use_parallel
     my_spmbatch_parallel(sublist,nsessions,task,datpath,params,save_intermediate_results)
 else
     my_spmbatch_noparallel(sublist,nsessions,task,datpath,params,save_intermediate_results)
