1031395b57b167d113e1fb90530eb44f528f3117
dr. Van Schuerbeek P <60138268+P-VS@users.noreply.github.com>
2023-10-12 15:40:57 +0200

    Add files via upload

---
+++ matlab/my_spmbatch/fmri_denoising-master/subfunc_ica_aroma/do_ica.m
@@ -0,0 +1,39 @@
+function do_ica(mask, tedata, n_sessions, t_r, ppparams)
+% DO_ICA Creates the parameter file necessary to run ICA with the GIFT toolbox
+% and performs ICA
+
+% mask = func mask
+% funcdata = func data
+% t_r = repetition time (read from json file)
+
+%% Estimate number of components (MDL - FWHM)
+dim_est_opts.method = 2; %MDL
+dim_est_opts.fwhm = [5,5,5]; %FWHM
+comp_est = 0;
+
+for ie=ppparams.echoes
+    [tcomp_est, mdl, aic, kic] = icatb_estimate_dimension(tedata{ie}.wfuncdat, mask, 'double', dim_est_opts);  
+
+    comp_est = comp_est + tcomp_est;
+end
+
+comp_est = comp_est / numel(tedata);
+
+%% Adapt template parameter to specific subject data
+
+icatb_defaults;
+
+% Load template parameters .mat file
+icaparms_file = ica_aroma_make_gift_parameters(t_r,n_sessions,mask,comp_est,ppparams);
+
+%% Set up ICA
+icaparam_file = icatb_setup_analysis(icaparms_file);
+
+load(icaparam_file);
+
+%% Run Analysis (All steps)
+sesInfo = icatb_runAnalysis(sesInfo, 1);
+
+end
+
+
