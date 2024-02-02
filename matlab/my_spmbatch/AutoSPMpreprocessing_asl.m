dbe97d76ff7fd730866aede3e253089a7a1e74bf
dr. Van Schuerbeek P <60138268+P-VS@users.noreply.github.com>
2024-02-02 12:26:20 +0100

    Added: smoothing of CBF maps

--- matlab/my_spmbatch/AutoSPMpreprocessing_asl.m
+++ matlab/my_spmbatch/AutoSPMpreprocessing_asl.m
@@ -29,13 +29,13 @@ params.spm_path = '/Users/accurad/Library/CloudStorage/OneDrive-Personal/Matlab/
 
 datpath = '/Volumes/LaCie/UZ_Brussel/ME_fMRI_studie_Linde/Data';
 
-sublist = [5,8:11,13:14,16:20];%list with subject id of those to preprocess separated by , (e.g. [1,2,3,4]) or alternatively use sublist = [first_sub:1:last_sub]
+sublist = [1:5,8:11,13:14,16:20];%list with subject id of those to preprocess separated by , (e.g. [1,2,3,4]) or alternatively use sublist = [first_sub:1:last_sub]
 nsessions = [2]; %nsessions>0
 
 params.multiple_scans = false;
 params.num_scans = [1]; %File names are sub##_asl-#_...
 
-params.save_folder = 'preproc_aslge_gm';
+params.save_folder = 'preproc_aslge_tm';
 
 params.use_parallel = true; 
 params.maxprocesses = 4; %Best not too high to avoid memory problems
@@ -53,7 +53,7 @@ params.reorient = true; % align data with MNI template to improve normalization
 %% In case data already processsed at the GE scanner (PCASL scan)
 
     params.aslge.do_cbfmapping = true;
-    params.aslge.T1correctionM0 = 'average_GM'; %T1 correction of M0scan: 'tisssue_maps', 'T1_map', 'average_GM', 'average_WM'
+    params.aslge.T1correctionM0 = 'tisssue_maps'; %T1 correction of M0scan: 'tisssue_maps', 'T1_map', 'average_GM', 'average_WM'
 
 %% Segmentation
 
@@ -64,6 +64,10 @@ params.reorient = true; % align data with MNI template to improve normalization
     params.asl.do_normalization = true;
     params.asl.normvox = [2 2 2];
   
+%% Smoothing
+    params.asl.do_smoothing = true;
+    params.asl.smoothfwhm = 6;
+
 %% BE CAREFUL WITH CHANGING THE CODE BELOW THIS LINE !!
 %---------------------------------------------------------------------------------------
 
