e84fbdb0f2ba4a892283fd95c90eae575d5ff6ba
dr. Van Schuerbeek P. <60138268+P-VS@users.noreply.github.com>
2025-01-09 08:13:02 +0100

    Add files via upload

--- matlab/my_spmbatch/my_spmbatch_subfuncs/my_spmbatch_preprocfunc.m
+++ matlab/my_spmbatch/my_spmbatch_subfuncs/my_spmbatch_preprocfunc.m
@@ -42,84 +42,90 @@ if params.func.do_echocombination || contains(ppparams.func(1).prefix,'c')
 end  
 
 %% Slice time correction
-for ie=ppparams.echoes
-    if params.func.do_slicetime  && ~contains(ppparams.func(ie).prefix,'a')
-        Vfunc = spm_vol(fullfile(ppparams.subfuncdir,[ppparams.func(ie).prefix ppparams.func(ie).funcfile]));
-        tdim = numel(Vfunc);
-        nvols = params.loadmaxvols;
-
-        for ti=1:nvols:tdim
-            if ti+nvols>tdim, nvols=tdim-ti+1; end
-            tVfunc = Vfunc(ti:ti+nvols-1);
-
-            fprintf(['Do slice time correction echo ' num2str(ie) ' vol ' num2str(ti) '-' num2str(ti+nvols-1) '\n'])
-
-            funcdat = spm_read_vols(tVfunc);
-            
-            if ~isfield(ppparams,'SliceTimes')
-                jsondat = fileread(ppparams.func(ie).jsonfile);
-                jsondat = jsondecode(jsondat);
-            
-                ppparams.tr = jsondat.RepetitionTime;
-                nsl=tVfunc(1).dim(3);
+try
+    for ie=ppparams.echoes
+        if params.func.do_slicetime  && ~contains(ppparams.func(ie).prefix,'a')
+            Vfunc = spm_vol(fullfile(ppparams.subfuncdir,[ppparams.func(ie).prefix ppparams.func(ie).funcfile]));
+            tdim = numel(Vfunc);
+            nvols = params.loadmaxvols;
+    
+            for ti=1:nvols:tdim
+                if ti+nvols>tdim, nvols=tdim-ti+1; end
+                tVfunc = Vfunc(ti:ti+nvols-1);
+    
+                fprintf(['Do slice time correction echo ' num2str(ie) ' vol ' num2str(ti) '-' num2str(ti+nvols-1) '\n'])
+    
+                funcdat = spm_read_vols(tVfunc);
                 
-                if isfield(jsondat,'SliceTiming'), ppparams.SliceTimes = jsondat.SliceTiming; else ppparams.SliceTimes = []; end
-                if ~(numel(ppparams.SliceTimes)==nsl), ppparams.SliceTimes = []; end
-
-                if params.func.isaslbold
-                    if isfield(jsondat,'LabelingDuration'), params.asl.LabelingDuration=jsondat.LabelingDuration; end
-                    if isfield(jsondat,'PostLabelDelay'), params.asl.PostLabelDelay=jsondat.PostLabelDelay; end
-                end
+                if ~isfield(ppparams,'SliceTimes')
+                    jsondat = fileread(ppparams.func(ie).jsonfile);
+                    jsondat = jsondecode(jsondat);
                 
-                if isempty(ppparams.SliceTimes)
-                    if isfield(jsondat,'MultibandAccelerationFactor')
-                        hbf = jsondat.MultibandAccelerationFactor; 
-                        nslex = ceil(nsl/hbf);
-                        isl = zeros([1,nslex]);
-                        isl(1:2:nslex)=[0:1:(nslex-1)/2];
-                        isl(2:2:nslex)=[ceil(nslex/2):1:nslex-1];
-                        isl=repmat(isl,[1,hbf]);
-                    else 
-                        isl = [1:2:nsl 2:2:nsl];
-                        isl = isl-1;
-                        nslex = nsl;
-                    end
-            
+                    ppparams.tr = jsondat.RepetitionTime;
+                    nsl=tVfunc(1).dim(3);
+                    
+                    if isfield(jsondat,'SliceTiming'), ppparams.SliceTimes = jsondat.SliceTiming; else ppparams.SliceTimes = []; end
+                    if ~(numel(ppparams.SliceTimes)==nsl), ppparams.SliceTimes = []; end
+    
                     if params.func.isaslbold
-                        TA = ppparams.tr-params.asl.LabelingDuration-params.asl.PostLabelDelay;
-                    else
-                        TA = ppparams.tr;
+                        if isfield(jsondat,'LabelingDuration'), params.asl.LabelingDuration=jsondat.LabelingDuration; end
+                        if isfield(jsondat,'PostLabelDelay'), params.asl.PostLabelDelay=jsondat.PostLabelDelay; end
                     end
-
-                    ppparams.SliceTimes = isl*TA/nslex;
-                elseif params.func.isaslbold
-                    TA = ppparams.tr-params.asl.LabelingDuration-params.asl.PostLabelDelay;
-
-                    ppparams.SliceTimes = ppparams.SliceTimes * TA/ppparams.tr;
+                    
+                    if isempty(ppparams.SliceTimes)
+                        if isfield(jsondat,'MultibandAccelerationFactor')
+                            hbf = jsondat.MultibandAccelerationFactor; 
+                            nslex = ceil(nsl/hbf);
+                            isl = zeros([1,nslex]);
+                            isl(1:2:nslex)=[0:1:(nslex-1)/2];
+                            isl(2:2:nslex)=[ceil(nslex/2):1:nslex-1];
+                            isl=repmat(isl,[1,hbf]);
+                            isl = isl(1:nsl);
+                        else 
+                            isl = [1:2:nsl 2:2:nsl];
+                            isl = isl-1;
+                            nslex = nsl;
+                        end
+                
+                        if params.func.isaslbold
+                            TA = ppparams.tr-params.asl.LabelingDuration-params.asl.PostLabelDelay;
+                        else
+                            TA = ppparams.tr;
+                        end
+    
+                        ppparams.SliceTimes = isl*TA/nslex;
+                    elseif params.func.isaslbold
+                        TA = ppparams.tr-params.asl.LabelingDuration-params.asl.PostLabelDelay;
+    
+                        ppparams.SliceTimes = ppparams.SliceTimes * TA/ppparams.tr;
+                    end
+    
+                    if params.func.isaslbold, ppparams.SliceTimes = params.asl.LabelingDuration+params.asl.PostLabelDelay+ppparams.SliceTimes; end
                 end
-
-                if params.func.isaslbold, ppparams.SliceTimes = params.asl.LabelingDuration+params.asl.PostLabelDelay+ppparams.SliceTimes; end
-            end
+                
+                funcdat=my_spmbatch_st(funcdat,tVfunc,ppparams.SliceTimes,ppparams.tr);
             
-            funcdat=my_spmbatch_st(funcdat,tVfunc,ppparams.SliceTimes,ppparams.tr);
-        
-            for iv=1:nvols
-                tVfunc(iv).fname = fullfile(ppparams.subfuncdir,['a' ppparams.func(ie).prefix ppparams.func(ie).funcfile]);
-                tVfunc(iv).descrip = 'my_spmbatch';
-                tVfunc(iv).n = [ti+iv-1 1];
+                for iv=1:nvols
+                    tVfunc(iv).fname = fullfile(ppparams.subfuncdir,['a' ppparams.func(ie).prefix ppparams.func(ie).funcfile]);
+                    tVfunc(iv).descrip = 'my_spmbatch';
+                    tVfunc(iv).n = [ti+iv-1 1];
+                
+                    tVfunc(iv) = spm_write_vol(tVfunc(iv),funcdat(:,:,:,iv));
+                end
             
-                tVfunc(iv) = spm_write_vol(tVfunc(iv),funcdat(:,:,:,iv));
+                clear funcdat tVfunc
             end
-        
-            clear funcdat tVfunc
+    
+            clear Vfunc
+            
+            ppparams.func(ie).prefix = ['a' ppparams.func(ie).prefix];
+            
+            delfiles{numel(delfiles)+1} = {fullfile(ppparams.subfuncdir,[ppparams.func(ie).prefix ppparams.func(ie).funcfile])};
         end
-
-        clear Vfunc
-        
-        ppparams.func(ie).prefix = ['a' ppparams.func(ie).prefix];
-        
-        delfiles{numel(delfiles)+1} = {fullfile(ppparams.subfuncdir,[ppparams.func(ie).prefix ppparams.func(ie).funcfile])};
     end
+catch e
+    fprintf('\nPP_Error\n');
+    fprintf('\nThe error was: \n%s\n',e.message)
 end
  
 %% Normalization of the func data
