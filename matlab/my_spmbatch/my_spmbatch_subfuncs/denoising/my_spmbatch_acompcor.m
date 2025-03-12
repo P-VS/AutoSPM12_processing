72109da7d83aa9cbc94b368548e68e47d860b37a
dr. Van Schuerbeek P. <60138268+P-VS@users.noreply.github.com>
2025-03-12 13:18:43 +0100

    Add files via upload

--- matlab/my_spmbatch/my_spmbatch_subfuncs/denoising/my_spmbatch_acompcor.m
+++ matlab/my_spmbatch/my_spmbatch_subfuncs/denoising/my_spmbatch_acompcor.m
@@ -2,10 +2,15 @@ function [ppparams,keepfiles] = my_spmbatch_acompcor(funcdat,ppparams,params,kee
 
 if isfield(ppparams,'der_file')
     confounds = load(ppparams.der_file);
+    [~,cfname,~] = fileparts(ppparams.der_file);
 elseif isfield(ppparams,'rp_file')
     confounds = load(ppparams.rp_file);
+    [~,cfname,~] = fileparts(ppparams.rp_file);
 else
     confounds = [];
+    tcfname = [ppparams.func(ppparams.echoes(1)).prefix ppparams.func(ppparams.echoes(1)).funcfile];
+    cfname = split(tcfname,'.nii');
+    cfname = cfname{1};
 end
 
 GM = spm_vol(ppparams.fc1im);
@@ -26,12 +31,17 @@ csfdat(csfdat>0.0)=1;
 
 acc_confounds = fmri_acompcor(funcdat(:,:,:,:),{csfdat},params.denoise.Ncomponents,'confounds',confounds,'filter',[],'PolOrder',1);
 
-accf = ['acc_' ppparams.func(ppparams.echoes(1)).prefix ppparams.func(ppparams.echoes(1)).funcfile];
+if ~isempty(confounds)
+    new_confounds = cat(2,confounds,acc_confounds);
+else
+    new_confounds = acc_confounds;
+end
+
+accf = ['acc_' cfname];
 accname = split(accf,'_echo-');
-accname = split(accf,'.nii');
 ppparams.acc_file = fullfile(ppparams.subfuncdir,[accname{1} '.txt']);
 
-writematrix(acc_confounds,ppparams.acc_file,'Delimiter','tab');
+writematrix(new_confounds,ppparams.acc_file,'Delimiter','tab');
 
 keepfiles{numel(keepfiles)+1} = {ppparams.acc_file};
 
